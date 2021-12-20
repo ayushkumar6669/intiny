0fb6c629cc7bdc7b765c2840297fada40c86a9bf
Ayush Kumar <raj6669ayush@gmail.com>
2021-12-20 22:53:33 +0530

    Google Auth resolve

--- controller/googleAuth.js
+++ controller/googleAuth.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: clientId,
         clientSecret: clientSecreT,
-        callbackURL: "http://localhost:8000/google/callback"
+        callbackURL: "https://gdfxsfsf.herokuapp.com/google/callback"
     }, (accessToken, refreshToken, profile, done) => {
         console.log(profile.emails[0].value);
 
