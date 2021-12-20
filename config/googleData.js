5ad381ff6d2f5d935e5408276f0de2a0c74bd83d
Ayush Kumar <raj6669ayush@gmail.com>
2021-12-20 22:46:30 +0530

    Google Auth

--- config/googleData.js
+++ config/googleData.js
@@ -1,2 +1,2 @@
-module.exports.clientId = "<Add Your Client ID>";
-module.exports.clientSecret = "<Add Your Client Secret>";
+module.exports.clientId = "801021841088-8irnn3a7fda1d39dqicmpiii1tck5ug4.apps.googleusercontent.com";
+module.exports.clientSecret = "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";
